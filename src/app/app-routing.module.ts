a9d002b100c3e598b28316c4d0eead1dd6ba95a0
Iliya-creator <78891186+Iliya-creator@users.noreply.github.com>
2022-10-13 20:22:36 -0700

    ADD: following along with Crystal's code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ToDoFormComponent } from './to-do-form/to-do-form.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+
+const routes: Routes = [
+  {
+    path: 'todos',
+    component: ToDoListComponent,
+    children: [
+      {
+        path: 'add',
+        component: ToDoFormComponent,
+      },
+    ],
+  },
+  { path: '', redirectTo: 'todos', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { ToDoFormComponent } from './to-do-form/to-do-form.component';
 import { HeaderComponent } from './header/header.component';
-import { ToDoComponent } from './to-do/to-do.component';
+import { ToDoComponent } from './to-do-list/to-do/to-do.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { ToDoComponent } from './to-do/to-do.component';
     ToDoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
